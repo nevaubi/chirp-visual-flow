a1374decc9a071b6ac4d7b4dc42b240e469d02e5
nevaubi <notreelai@gmail.com>
2025-05-21 12:36:49 -0500

    Update index.ts

--- supabase/functions/asynch-database-AI/index.ts
+++ supabase/functions/asynch-database-AI/index.ts
@@ -90,9 +90,9 @@ async function runTwitterXScraper(): Promise<any[]> {
     "include:nativeretweets": false,
     "lang": "en",
     "list": "1905144063638073662",
-    "maxItems": 200,
+    "maxItems": 50,
     "queryType": "Top",
-    "within_time": "60d"
+    "within_time": "1d"
   };
 
   console.log("Calling Twitter scraper with params:", JSON.stringify(params));
