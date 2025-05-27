1bb17797ab7c7f83e5f85de235660912ed223f26
nevaubi <notreelai@gmail.com>
2025-05-27 11:49:02 -0500

    Move console override to top

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
+/* eslint-disable no-console */
+console.log = () => {};
+console.error = () => {};
+console.warn = () => {};
+/* eslint-enable no-console */
+
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(<App />)
